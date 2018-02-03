c745bdb701c7ed603b690be087421a49b95fe4f8
Qiaolun Zhang <qiaolunzhang@gmail.com>
2018-02-03 20:13:11 +0800

    two code to download

---
+++ getfile_from_a_ website.py
@@ -0,0 +1,98 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+
+def getLoc(start_url):
+    try:
+        #start_url = 'http://ipads.se.sjtu.edu.cn/courses/ics/schedule.shtml'
+        kv = {'user-agent':'Mozilla/5.0'}
+        r = requests.get(start_url, headers=kv)
+        r.raise_for_status()
+    except:
+        print("爬取失败")
+        return ""
+    demo = r.text
+    soup = BeautifulSoup(demo, 'html.parser')
+    for link in soup.find_all('a'):
+        print(link.get('href'))
+    for link in soup.find_all('a'):
+        loc = link.get('href')
+        try:
+            flag = loc.find('pdf')
+            if (flag >= 0):
+                savePdf(loc)
+                continue
+        except:
+            continue
+        try:
+            flag = loc.find('ppt')
+            if (flag >= 0):
+                savePpt(loc)
+                continue
+        except:
+            continue
+        try:
+            flag = loc.find('txt')
+            if (flag >= 0):
+                saveTxt(loc)
+                continue
+        except:
+            continue
+    print("完成")
+
+
+
+    return 0
+
+def savePdf(loc):
+    #url = 'http://ipads.se.sjtu.edu.cn/courses/ics/'
+    #root = './/download//'
+    try:
+        kv = {'user-agent':'Mozilla/5.0'}
+        r = requests.get(url+loc, headers=kv)
+        path = root + 'pdf//' + loc.split('/')[-1]
+        with open(path, 'wb') as f:
+            f.write(r.content)
+            f.close()
+            print("一份pdf")
+    except:
+        return ""
+
+def savePpt(loc):
+    try:
+        kv = {'user-agent':'Mozilla/5.0'}
+        r = requests.get(url+loc, headers=kv)
+        path = root + 'ppt_pptx//' + loc.split('/')[-1]
+        with open(path, 'wb') as f:
+            f.write(r.content)
+            f.close()
+            print("一份ppt")
+    except:
+        return ""
+
+def saveTxt(loc):
+    try:
+        kv = {'user-agent':'Mozilla/5.0'}
+        r = requests.get(url+loc, headers=kv)
+        path = root + 'txt//' + loc.split('/')[-1]
+        with open(path, 'wb') as f:
+            f.write(r.content)
+            f.close()
+        print("一份txt")
+    except:
+        return ""
+
+if __name__ == '__main__':
+    start_url = "http://www.cs.cornell.edu/courses/cs1110/2017fa/lectures/index.php"
+    url = "http://www.cs.cornell.edu/courses/cs1110/2017fa/lectures/"
+    if not os.path.exists('.//download'):
+        os.mkdir('.//download')
+    root = './/download//'
+    if not os.path.exists(root+'pdf'):
+        os.mkdir(root+'pdf')
+    if not os.path.exists(root+'ppt_pptx'):
+        os.mkdir(root+'ppt_pptx')
+    if not os.path.exists(root+'txt'):
+        os.mkdir(root+'txt')
+    getLoc(start_url)
---
+++ getfile_from_single_webpage.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+
+def get_ref(start_url):
+    try:
+        kv = {'user-agent': 'Mozilla/5.0'}
+        r = requests.get(start_url, headers=kv)
+        r.raise_for_status()
+    except:
+        print("failed to get the start url")
+        return
+
+    text = r.text
+    soup = BeautifulSoup(text, 'html.parser')
+    for link in soup.find_all('a'):
+        href = link.get('herf')
+        file_name = href.split('/')[-1]
+        try:
+            if file_name.split('.')[-1] == 'pdf':
+                save_pdf(href)
+        except:
+            continue
+
+
+def save_pdf(href):
+    try:
+        kv = {'user-agent': 'Mozilla/5.0'}
+        r = requests.get(url+href, headers=kv)
+        path = root + href.split('/')[-1]
+        with open(path, 'wb') as f:
+            f.write(r.content)
+            f.close()
+        print("一份pdf")
+    except:
+        return ""
+
+
+if __name__ == '__main__':
+    start_url = 'http://www.cs.cornell.edu/courses/cs1110/2017fa/lectures/index.php'
+    url = 'http://www.cs.cornell.edu/courses/cs1110/2017fa/lectures/'
+    root = './/download-single//'
+    if not os.path.exists(root):
+        os.mkdir('.//download-single')
+    get_ref(start_url)
