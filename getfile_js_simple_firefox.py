dff94d2c7e75699e49cff1278675651b24ce3554
Qiaolun Zhang <qiaolunzhang@gmail.com>
2018-02-04 15:23:01 +0800

    getfile from website in js

---
+++ getfile_js_simple_firefox.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+from selenium import webdriver
+from time import sleep
+
+
+def get_ref(start_url):
+    try:
+        browser = webdriver.Firefox()
+        browser.get(start_url)
+        sleep(5)
+        text = browser.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+    except:
+        print("failed")
+        return
+
+    soup = BeautifulSoup(text, 'html.parser')
+    for link in soup.find_all('a'):
+        print(link.get('href'))
+    print('******************************************')
+    print('******************************************')
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        print(href)
+        try:
+            file_name = href.split('/')[-1]
+            # print(file_name)
+            # print(file_name.split('.'))
+            if file_name.split('.')[-1] == 'pdf':
+                save_pdf(href)
+        except:
+            continue
+
+
+def save_pdf(href):
+    try:
+        kv = {'user-agent': 'Mozilla/5.0'}
+        r = requests.get(url+href, headers=kv)
+        print(url+href)
+        path = root + href.split('/')[-1]
+        with open(path, 'wb') as f:
+            f.write(r.content)
+            f.close()
+        print("一份pdf")
+    except:
+        return ""
+
+
+if __name__ == '__main__':
+    start_url = 'http://www.cs.cornell.edu/courses/cs1112/2017fa/#templates/syllabus?section=lectures'
+    url = 'http://www.cs.cornell.edu/courses/cs1112/2017fa/'
+    root = './/download-single//'
+    if not os.path.exists(root):
+        os.mkdir('.//download-single')
+    get_ref(start_url)
